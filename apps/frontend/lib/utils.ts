7808d2f5e94b28f6fac3b3d74d07c6a596a2c670
lalitcap23 <lalitrajput232002@gmail.com>
2025-05-15 18:55:44 +0530

    auth

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -1,6 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+// Utils function for classname merging
+export function cn(...classes: (string | undefined | boolean)[]) {
+  return classes.filter(Boolean).join(" ");
 }
